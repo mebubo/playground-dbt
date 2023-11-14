0c1290c1fdce5430c750c1c11a749fdb10112752
Sergei Dolgov <dolgovs@gmail.com>
2023-11-14 02:13:47 +0000

    2019-{02,03} have a different schema, exclude them

--- generate.py
+++ generate.py
@@ -4,9 +4,8 @@ import duckdb
 def date_range(y1, m1, y2, m2) -> list[tuple[int, int]]:
     return [(y, m) for y in range(y1, y2+1) for m in range(1, 13) if (y, m) >= (y1, m1) and (y, m) < (y2, m2)]
 
-FHVHV_ALL = date_range(2019, 2, 2023, 9)
-# FHVHV_ALL = date_range(2019, 2, 2021, 9)
-# FHVHV_ALL = date_range(2021, 2, 2022, 4)
+# FHVHV_ALL = date_range(2019, 2, 2023, 9)
+FHVHV_ALL = date_range(2019, 4, 2023, 9)
 
 def urls() -> list[str]:
     return [ f"https://d37ci6vzurychx.cloudfront.net/trip-data/fhvhv_tripdata_{year}-{month:02d}.parquet" for year, month in FHVHV_ALL ]
